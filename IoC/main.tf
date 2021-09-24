67872ee2de2d8492a3aa2267f6d4dd9dbe20ca5e
Raniel Garcia <ranielgarcia2596@gmail.com>
2021-09-24 22:22:53 +0700

    Added main.tf

---
+++ IoC/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "Southeast Asia"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name                    = "weatherapi"
+    location                = azurerm_resource_group.tf_test.location
+    resource_group_name     = azurerm_resource_group.tf_test.name
+    
+    ip_address_type         = "public"
+    dns_name_label          = "raniel12345DNSTest"
+    os_type                 = "Linux"
+
+    container {
+        name                = "weatherapi"
+        image               = "raniel12345/weatherapi"
+        cpu                 = "1"
+        memory              = "1"
+
+        ports {
+            port            = 80
+            protocol        = "TCP"
+        }
+    }    
+}
